1f3e8f52e683ac418794ef563f958ffb3ae99719
mundoenrique <mundoenrique@hotmail.com>
2022-06-18 00:22:08 -0400

    Renderiza variables y estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,28 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import { Fragment } from 'react'
 import './App.css'
 
+const name = 'Enrique';
+const arr = [1, 2, 3, 4, 5, 6];
+const obj = {
+	name: 'yajaira',
+	edad: 40
+}
+
+function addNumbers(a, b) {
+	return a + b;
+}
+
 function App() {
-	const [count, setCount] = useState(0)
 
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>Hello Vite + React!</p>
-				<p>
-					<button type="button" onClick={() => setCount((count) => count + 1)}>
-						count is: {count}
-					</button>
-				</p>
-				<p>
-					Edit <code>App.jsx</code> and save to test HMR updates.
-				</p>
-				<p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-					</a>
-					{' | '}
-					<a
-						className="App-link"
-						href="https://vitejs.dev/guide/features.html"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Vite Docs
-					</a>
-				</p>
-			</header>
-		</div>
+		<Fragment>
+			<h1>Hola {name}</h1>
+			{/* <p>{subTitle}</p> */}
+			<p>Arreglo {arr}</p>
+			<p>Sumar números {addNumbers(4, 5)}</p>
+			<pre>Objeto = {JSON.stringify(obj, null, 2)}</pre>
+			{/* <p>Propiedad recibida = {propSent}</p> */}
+		</Fragment>
 	)
 }
 
